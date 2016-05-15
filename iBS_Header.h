204c7271345639a757e80c26041162dabf3c2692
NashBean <nashbean@gmail.com>
2016-05-15 13:37:29 -0400

    updated  iBS Header and added main

--- iBS_Header.h
+++ iBS_Header.h
@@ -3,21 +3,65 @@
 //  iBS_LIB
 //
 //  Created by nash on 5/15/16.
-//  Copyright 2016 __MyCompanyName__. All rights reserved.
+//  Copyright 2016 iBean Software. 
+//  All rights reserved.
+//  http://www.NashBean.github.io/index.html 
 //
+#ifndef iBS_Header_h
+#define iBS_Header_h
+
+
 namespace iBS 
 {
-    struct UnicodeInt
+    
+    struct uint 
     {
-        UnicodeInt()(ref=NULL;); 
-        UnicodeInt(int v)(ref=(uint32_t)v;); 
-        UnicodeInt(long v)(ref=(uint32_t)v;); 
-        UnicodeInt(char v)(ref=(uint32_t)v;); 
-        UnicodeInt(uint32_t v)(ref=v;); 
+        
+        uint():value(0){}; 
+        uint(int v):value((uint32_t)v){}; 
+        uint(long v):value((uint32_t)v){}; 
+        uint(char v):value((uint32_t)v){}; 
+        uint(uint const &v):value(v.value){}; 
+        uint(uint32_t v):value(v){}; 
+        
+        uint& operator=(uint unicode)
+        {
+            set(unicode.value); 
+            return *this;
+        };
+        uint& operator=(uint32_t unicode)
+        {
+            set(unicode); 
+            return *this;
+        };
+        
+        bool operator==(char c)
+        {
+            return (value == (uint32_t)c);
+        };
+        bool operator==(int i)
+        {
+            return (value == (uint32_t)i);
+        };
+        bool operator==(uint32_t unicode)
+        {
+            return (value == unicode);
+        };
+        
+        uint32_t get()
+        {
+            return value;
+        };
+        void set(uint32_t v)
+        {
+            value = v;
+        };
         
     private:    
-        uint32_t ref;
+        uint32_t value;
+        
     };
     
     
 }// end of iBS namespace
+#endif // iBS_Header_h
---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  iBS_LIB
+//
+//  Created by nash on 5/15/16.
+//  Copyright 2016 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+#include "iBS_Header.h"
+
+int main (int argc, const char * argv[])
+{
+    
+    iBS::uint testint = iBS::uint();
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

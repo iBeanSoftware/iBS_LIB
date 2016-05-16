af5b35f0d6b742936d332184c8421446692405c3
NashBean <nashbean@gmail.com>
2016-05-16 16:04:10 -0400

    updated Header to v0.4

--- iBS_Header.h
+++ iBS_Header.h
@@ -11,9 +11,11 @@
 #define iBS_Header_h
 
 #include <vector>
+#include <cwchar>
+
 
 const int iBS_HEADER_MAJOR_VERSION = 0;
-const int iBS_HEADER_MINOR_VERSION = 3;
+const int iBS_HEADER_MINOR_VERSION = 4;
 
 namespace iBS 
 {
@@ -37,6 +39,7 @@ namespace iBS
         bool operator==(char c) { std::string str; return (value == (uint32_t)c); };
         bool operator==(int i) { return (value == (uint32_t)i); };
         bool operator==(uint32_t unicode) { return (value == unicode); };
+        bool operator==(uint unicode) { return (value == unicode.get()); };
         
         uint32_t get() { return value; };
         void set(uint32_t v) { value = v; };
@@ -47,10 +50,74 @@ namespace iBS
     };
  
     //-----------------------------------------------
+    //inline function for uint 
+    //-----------------------------------------------
+    inline uint decode(std::vector<char>& c)
+    {
+        uint32_t result = uint32_t(0);
+        
+       //UnicodeInt result=0;
+        if (c.size()==0) { return uint(result);}
+        if (c.size()==1) { return uint(c[0]); }
+        if (c.size()==2) { result=c[0] & 0x1f; }
+        else if (c.size()==3) { result=c[0] & 0x0f; }
+        else if (c.size()==4) { result=c[0] & 0x07; }
+        else if (c.size()==5) { result=c[0] & 0x03; }
+        else if (c.size()==6) { result=c[0] & 0x01; }
+        for (size_t i=1; i<c.size(); ++i) 
+        { result=result<<6; result+=c[i] & 0x3f; }
+        
+        
+        return uint(result);
+    };
+    
+    inline uint decode(wchar_t& wch)
+    {
+      //  uint result = uint();
+        
+        
+        //*
+         char temp[6];
+         std::vector<char> ref;
+         std::mbstate_t state ;
+         int ret = std::wcrtomb((&temp[0]), wch, &state);
+             
+         ref.resize(ret);
+         for (short i=0; i<ret; ++i) 
+             ref[i]=temp[i];
+         //*/
+        return decode(ref);
+    };
+
+    //-----------------------------------------------
     //uchar = uint made to hold any char of the world
     //-----------------------------------------------
      struct uchar
     {
+        uchar():value(0){}; 
+        uchar(int v):value((uint32_t)v){}; 
+        uchar(long v):value((uint32_t)v){}; 
+        uchar(uint32_t v):value(v){}; 
+        uchar(uint v):value(v.get()){}; 
+        uchar(char v):value((uint32_t)v){}; 
+        uchar(uchar const &v):value(v.value){}; 
+        uchar(wchar_t wch):value(decode(wch)){}; 
+        
+        uchar& operator=(int unicode){ set((uint32_t)unicode); return *this; };
+        uchar& operator=(uint32_t unicode){ set(unicode); return *this; };
+        uchar& operator=(uint unicode) { set(unicode.get()); return *this; };
+        uchar& operator=(char ch) { set((uint32_t)ch); return *this; };
+        uchar& operator=(uchar uch) { set(uch.get()); return *this; };
+        uchar& operator=(wchar_t wch) { set(decode(wch).get()); return *this; };// use of Uchar.h
+        
+        bool operator==(int i) { return (value == i); };
+        bool operator==(uint32_t unicode) { return (value == unicode); };
+        bool operator==(uint unicode) { return (value == unicode); };
+        bool operator==(char c) { return (value == c); };
+        bool operator==(uchar uch) { return (value == uch.value); };
+        
+        uint32_t get() { return value.get(); };
+        void set(uint32_t v) { value = v; };
 
     private:    
         uint value;
